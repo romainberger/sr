2c95f6bbcdc2186c2edf24f1a3f0453e849e8113
Romain Berger <romain.berger@datadoghq.com>
2019-09-20 15:05:51 +0200

    data file for assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
+const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
 
+const DATA_FILE_PATH = path.join(__dirname, '_data/assets.json')
 const NODE_ENV = process.env.NODE_ENV || "development"
 
 module.exports = {
     mode: NODE_ENV,
     entry: {
+        index: path.join(__dirname, 'src/index.js'),
         transfer: path.join(__dirname, 'src/transfer.js'),
     },
     output: {
-        filename: NODE_ENV === 'production' ? '[name]-[chunkhash].js' : '[name].js',
+        filename: NODE_ENV === 'production' ? '[name]-[chunkhash].js' : '[name]-[chunkhash].js',
         path: path.join(__dirname, 'build'),
         publicPath: './build/',
     },
@@ -29,5 +32,26 @@ module.exports = {
         new webpack.DefinePlugin({
             __DEV__: NODE_ENV === "development",
         }),
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap('AfterEmitPlugin', compilation => {
+                    const assets = Object.keys(compilation.assets).reduce((acc, asset) => {
+                        const name = asset.split('-')[0]
+                        acc[name] = `build/${asset}`
+
+                        return acc
+                    }, {})
+
+                    fs.writeFile(DATA_FILE_PATH, JSON.stringify(assets, null, 2), err => {
+                        if (err) {
+                            console.error('Error writing asset file', err)
+                            return
+                        }
+
+                        console.log('Asset file written')
+                    })
+                })
+            },
+        },
     ],
 }
