d4561ac0c693eea70bd2383d64519efde055abbd
Romain Berger <romain.berger@datadoghq.com>
2019-12-18 11:28:42 +0100

    Add validate json CI

--- scripts/validate-json.js
+++ scripts/validate-json.js
@@ -7,11 +7,21 @@ const files = [
     'update-win.json',
 ]
 
+let valid = true
+
 files.forEach(file => {
     try {
         require(path.join(__dirname, '../', file))
     }
     catch(e) {
-        process.exit(1)
+        console.error(`Invalid json file ${file}`);
+        valid = false
     }
 })
+
+if (!valid) {
+    process.exit(1)
+}
+else {
+    console.log('All good');
+}
